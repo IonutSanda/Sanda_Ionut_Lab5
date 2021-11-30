fee1cb0dc560c2d7967c75874d70305f24a656d2
Ionut Sanda <ionut.sanda@siemens.com>
2021-11-30 23:59:31 +0200

    Sanda_Ionut_Lab7

--- Sanda_Ionut_Lab5/MainWindow.xaml.cs
+++ Sanda_Ionut_Lab5/MainWindow.xaml.cs
@@ -318,6 +318,35 @@ namespace Sanda_Ionut_Lab5
                          };
         customerOrdersViewSource.Source = queryOrder.ToList();
     }
+        private void SetValidationBinding()
+        {
+            Binding firstNameValidationBinding = new Binding();
+            firstNameValidationBinding.Source = customerViewSource;
+            firstNameValidationBinding.Path = new PropertyPath("FirstName");
+            firstNameValidationBinding.NotifyOnValidationError = true;
+            firstNameValidationBinding.Mode = BindingMode.TwoWay;
+            firstNameValidationBinding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            firstNameValidationBinding.ValidationRules.Add(new StringNotEmpty());
+            firstNameTextBox.SetBinding(TextBox.TextProperty, firstNameValidationBinding);
+            Binding lastNameValidationBinding = new Binding();
+            lastNameValidationBinding.Source = customerViewSource;
+            lastNameValidationBinding.Path = new PropertyPath("LastName");
+            lastNameValidationBinding.NotifyOnValidationError = true;
+            lastNameValidationBinding.Mode = BindingMode.TwoWay;
+            lastNameValidationBinding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            lastNameValidationBinding.ValidationRules.Add(new StringMinLengthValidator());
+            lastNameTextBox.SetBinding(TextBox.TextProperty, lastNameValidationBinding);
+        }
+
+        private void btnCustEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+                action = ActionState.Edit;
+                BindingOperations.ClearBinding(firstNameTextBox, TextBox.TextProperty);
+                BindingOperations.ClearBinding(lastNameTextBox, TextBox.TextProperty);
+                SetValidationBinding();
+            
+        }
     }
 
 }
